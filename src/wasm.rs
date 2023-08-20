2a26a481758bf5618998206b72560ed1cf2dc332
Stephan Hügel <shugel@tcd.ie>
2023-08-20 23:19:29 +0100

    More concise WASM Array conversion

--- src/wasm.rs
+++ src/wasm.rs
@@ -3,27 +3,18 @@ use js_sys::Array;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsValue;
 
+/// convert individual ckmeans result classes to WASM-compatible Arrays
 fn inner_vec_to_js_array(data: Vec<f64>) -> Array {
-    let array = Array::new();
-    for num in data {
-        array.push(&JsValue::from_f64(num));
-    }
-    array
+    data.into_iter().map(JsValue::from).collect()
 }
 
-// Convert a ckmeans result to an Array suitable for use by a JS function
-pub fn wrapper_vec_to_js_array(data: Vec<Vec<f64>>) -> Array {
-    let outer_array = Array::new();
-    for inner_vec in data {
-        outer_array.push(&inner_vec_to_js_array(inner_vec));
-    }
-    outer_array
+/// Convert a ckmeans result to an Array suitable for use by a JS function
+fn wrapper_vec_to_js_array(data: Vec<Vec<f64>>) -> Array {
+    data.into_iter().map(inner_vec_to_js_array).collect()
 }
 
 #[wasm_bindgen]
+/// A WASM wrapper for ckmeans
 pub fn ckmeans_wasm(data: &[f64], nclusters: u8) -> Result<Array, JsError> {
-    // Your implementation here...
-
-    let res = ckmeans(data, nclusters).unwrap();
-    Ok(wrapper_vec_to_js_array(res))
+    Ok(wrapper_vec_to_js_array(ckmeans(data, nclusters)?))
 }
