51a4bacbeb69159e4a2736dc5f9177a96f801026
Stephan Hügel <shugel@tcd.ie>
2023-08-20 23:53:28 +0100

    Add roundbreaks wasm wrapper

--- src/wasm.rs
+++ src/wasm.rs
@@ -1,4 +1,5 @@
 use crate::ckmeans;
+use crate::roundbreaks;
 use js_sys::Array;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsValue;
@@ -17,4 +18,10 @@ fn wrapper_vec_to_js_array(data: Vec<Vec<f64>>) -> Array {
 /// A WASM wrapper for ckmeans
 pub fn ckmeans_wasm(data: &[f64], nclusters: u8) -> Result<Array, JsError> {
     Ok(wrapper_vec_to_js_array(ckmeans(data, nclusters)?))
+}
+
+#[wasm_bindgen]
+/// A WASM wrapper for roundbreaks
+pub fn roundbreaks_wasm(data: &[f64], nclusters: u8) -> Result<Array, JsError> {
+    Ok(inner_vec_to_js_array(roundbreaks(data, nclusters)?))
 }
