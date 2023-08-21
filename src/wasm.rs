0237e122ee6a367278429a39d5b6cdc8be97230f
Stephan Hügel <shugel@tcd.ie>
2023-08-21 10:55:58 +0100

    Return (Arrays of) Float64Array Views from WASM

--- src/wasm.rs
+++ src/wasm.rs
@@ -1,17 +1,18 @@
 use crate::ckmeans;
 use crate::roundbreaks;
 use js_sys::Array;
+use js_sys::Float64Array;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsValue;
 
 /// convert individual ckmeans result classes to WASM-compatible Arrays
-fn inner_vec_to_js_array(data: Vec<f64>) -> Array {
-    data.into_iter().map(JsValue::from).collect()
+fn inner_vec_to_js_array(data: &[f64]) -> Float64Array {
+    unsafe { Float64Array::view(data) }
 }
 
 /// Convert a ckmeans result to an Array suitable for use by a JS function
 fn wrapper_vec_to_js_array(data: Vec<Vec<f64>>) -> Array {
-    data.into_iter().map(inner_vec_to_js_array).collect()
+    data.iter().map(|v| inner_vec_to_js_array(v)).collect()
 }
 
 #[wasm_bindgen]
@@ -22,6 +23,6 @@ pub fn ckmeans_wasm(data: &[f64], nclusters: u8) -> Result<Array, JsError> {
 
 #[wasm_bindgen]
 /// A WASM wrapper for roundbreaks
-pub fn roundbreaks_wasm(data: &[f64], nclusters: u8) -> Result<Array, JsError> {
-    Ok(inner_vec_to_js_array(roundbreaks(data, nclusters)?))
+pub fn roundbreaks_wasm(data: &[f64], nclusters: u8) -> Result<Float64Array, JsError> {
+    Ok(inner_vec_to_js_array(&roundbreaks(data, nclusters)?))
 }
