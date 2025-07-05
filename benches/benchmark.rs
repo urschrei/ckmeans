4ea9c828ae782fe031bcc2a3cce266456eb82c5d
Stephan Hügel <shugel@tcd.ie>
2025-07-05 14:42:10 +0100

    Dependency update

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -3,16 +3,18 @@ extern crate criterion;
 extern crate ckmeans;
 extern crate rand;
 
-use rand::{distributions::Uniform, Rng}; // 0.6.5
+use rand::Rng; // 0.6.5
 use rand_distr::Normal;
+use rand_distr::Uniform;
+use std::hint::black_box;
 
 use ckmeans::ckmeans;
-use criterion::{black_box, Criterion};
+use criterion::Criterion;
 
 fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("110k i32, Uniform: 0 - 250", |bencher| {
-        let mut rng = rand::thread_rng();
-        let range = Uniform::new(0, 250);
+        let mut rng = rand::rng();
+        let range = Uniform::new(0, 250).unwrap();
 
         let data: Vec<i32> = (0..110_000).map(|_| rng.sample(range)).collect();
         bencher.iter(|| {
@@ -21,7 +23,7 @@ fn criterion_benchmark(c: &mut Criterion) {
     });
 
     c.bench_function("110k f64, Gaussian: mu = 3, sigma = 1", |bencher| {
-        let mut rng = rand::thread_rng();
+        let mut rng = rand::rng();
         let range = Normal::new(3.0, 1.0).unwrap();
 
         let data: Vec<f64> = (0..110_000).map(|_| rng.sample(range)).collect();
